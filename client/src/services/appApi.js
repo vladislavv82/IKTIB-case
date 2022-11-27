0c94a4474a480f04c420634cf4e91e571240ecc1
vladislavv82 <vladislav.akopian82@gmail.com>
2022-11-27 09:22:46 +0300

    prod

--- client/src/services/appApi.js
+++ client/src/services/appApi.js
@@ -1,34 +1,33 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 // define a service user a base URL
 
 const appApi = createApi({
-    reducerPath: "appApi",
-    baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:5001",
+  reducerPath: 'appApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://79.143.24.10:5001',
+  }),
+
+  endpoints: (builder) => ({
+    // login
+    loginUser: builder.mutation({
+      query: (user) => ({
+        url: '/users/login',
+        method: 'POST',
+        body: user,
+      }),
     }),
 
-    endpoints: (builder) => ({
+    // logout
 
-        // login
-        loginUser: builder.mutation({
-            query: (user) => ({
-                url: "/users/login",
-                method: "POST",
-                body: user,
-            }),
-        }),
-
-        // logout
-
-        logoutUser: builder.mutation({
-            query: (payload) => ({
-                url: "/logout",
-                method: "DELETE",
-                body: payload,
-            }),
-        }),
+    logoutUser: builder.mutation({
+      query: (payload) => ({
+        url: '/logout',
+        method: 'DELETE',
+        body: payload,
+      }),
     }),
+  }),
 });
 
 export const { useLoginUserMutation, useLogoutUserMutation } = appApi;
